d0a0114688a538c306ecae668285e01571b01a68
puzzle <2015534205@qq.com>
2026-04-14 23:45:53 +0800

    codex更改

--- library/inc/LS2K0300_I2C.h
+++ library/inc/LS2K0300_I2C.h
@@ -3,18 +3,21 @@
 
 #include "LS2K0300_GPIO.h"
 #include <stdint.h>
+#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef struct {
-    gpio_pin_t scl;
-    gpio_pin_t sda;
+    ls2k0300_gpio_t scl;
+    ls2k0300_gpio_t sda;
     uint8_t addr;
+    bool initialized;
 } ls2k0300_i2c_t;
 
 int ls2k0300_i2c_init(ls2k0300_i2c_t* i2c, gpio_pin_t scl, gpio_pin_t sda, uint8_t addr);
+void ls2k0300_i2c_deinit(ls2k0300_i2c_t* i2c);
 uint8_t ls2k0300_i2c_read_byte(ls2k0300_i2c_t* i2c, const uint8_t reg, uint8_t* val);
 uint8_t ls2k0300_i2c_write_byte(ls2k0300_i2c_t* i2c, const uint8_t reg, uint8_t val);
 uint8_t ls2k0300_i2c_read_n_byte(ls2k0300_i2c_t* i2c, const uint8_t reg, uint8_t* buf, uint8_t len);
